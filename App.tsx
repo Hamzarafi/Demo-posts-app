0a48c54a243db386e56d37ef2fd61bb617991daa
Hamza <hamza@mindbehind.com>
2022-07-02 16:44:06 +0300

    Add context

--- App.tsx
+++ App.tsx
@@ -3,14 +3,20 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation";
+import { MyGlobalContext } from "./hooks/globalContext";
+import { useState } from "react";
 
 export default function App() {
   const colorScheme = useColorScheme();
 
+  const [posts, setPosts] = useState<PostType[]>([]);
+
   return (
-    <SafeAreaProvider>
-      <Navigation colorScheme={colorScheme} />
-      <StatusBar />
-    </SafeAreaProvider>
+    <MyGlobalContext.Provider value={{ posts, setPosts }}>
+      <SafeAreaProvider>
+        <Navigation colorScheme={colorScheme} />
+        <StatusBar />
+      </SafeAreaProvider>
+    </MyGlobalContext.Provider>
   );
 }
